4d52a5d47c3adff0cd98f3df9c5dc8ded0e29f15
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2020-05-24 11:49:21 +0300

    .

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,9 @@
 const Discord = require('discord.js')
 const {RichEmbed } = require('discord.js');
 module.exports = {
-    name: 'sample',
-    execute(message, args, useLang, usePrefix, useAdminChat, useAnnounceChat, server, bot, version){
-        if (message.member.voice.channel) {
-            const connection = await message.member.voice.channel.join();
-          } else {
-            message.reply('You need to join a voice channel first!');
-          }
+    name: 'test',
+    async execute(message, args, useLang, usePrefix, useAdminChat, useAnnounceChat, server, bot, version){
+        
         message.delete();
     }
 }
