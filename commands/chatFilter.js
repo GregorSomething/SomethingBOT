4bddfbf17c118205b4a50ed39a8d78288c1d5032
GregorSomething <gregor.suurvarik@gmail.com>
2021-04-06 12:52:41 +0300

    Thing

--- commands/chatFilter.js
+++ commands/chatFilter.js
@@ -1,22 +1,12 @@
 const { Message } = require('discord.js')
 const main = require('../index.js')
 const sys = main.sys
-
-sys
 module.exports = {
     name: 'chatFilter',
     alias: ['cf', 'filter'],
     perms: [],
     async execute(message, args, config, bot, sys) {
         if (args[1] == 'init') sys.emit('msgContainsAdd', args[2], message.guild.id)    
-    },
-    test: {
-        et: "Jees",
-        en: "Huh"
-    },
-    test2: {
-        et: "FFFF",
-        en: "GGG"
     },
     help: {
         et: {
